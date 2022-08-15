975574146bea9b91dd9433568a1368186bbabcb8
vlad2000121 <vi281412@gmail.com>
2022-08-15 17:52:31 +0300

    Добавьте файлы проекта.

---
+++ InspectionAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace InspectionAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+
+    }
+}
